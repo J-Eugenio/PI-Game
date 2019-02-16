03ff4def3853604c2267d81b1a5017d730d5d7d8
J-Eugenio <eugeniopb4@gmail.com>
2019-02-16 14:46:29 -0300

    Primeiros Scripts de test

---
+++ Projeto/PI-Game/Assets/Scripts/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LoginController : MonoBehaviour {
+
+    private const string Login = "Aljoker";
+    private const string Pass = "123";
+
+    [SerializeField]
+    private InputField usuario = null;
+    [SerializeField]
+    private InputField senha = null;
+    [SerializeField]
+    private Text FeedBackMsg = null;
+    [SerializeField]
+    private Toggle LembrarDados = null;
+
+    
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
