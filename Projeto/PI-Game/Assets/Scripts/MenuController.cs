51b611e4ed171484af2d3ae18db9eaf2b11970dd
DawnGrade <37818718+ThiagoAlencar12@users.noreply.github.com>
2019-02-18 20:33:53 -0300

    Menu Principal

---
+++ Projeto/PI-Game/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void carregarScena()
+    {
+        StartCoroutine(CarregaScene());
+    }
+    IEnumerator CarregaScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Level1");
+    }
+}
--- Projeto/pi/conexao/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$server = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$db = "pi";
-	$connect = mysqli_connect($server, $usuario, $senha, $db) or die("Erro ao conectar no Banco");
-?>
