9f71920e1078a091d6a420358ed389b46740d21d
J-Eugenio <37482325+J-Eugenio@users.noreply.github.com>
2019-03-16 15:11:26 -0300

    imagens do ceanrio

---
+++ Projeto/PI-Game/Assets/Scripts/TutorialController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public void Collider2D ()
+    {
+
+    }
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
