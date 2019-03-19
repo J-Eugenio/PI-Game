a78a3f8b51301ddec30133fc2c0a2ab5bfc6f08c
J-Eugenio <eugeniopb4@gmail.com>
2019-03-19 19:52:20 -0300

    Inimigo, a ajustar

--- Projeto/PI-Game/Assets/Scripts/inimigo.cs
+++ Projeto/PI-Game/Assets/Scripts/inimigo.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class inimigo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidade;
+    public bool plantaforma = false;
+    Rigidbody2D ini;
+    bool facingRight = false;
+    bool noChao = false;
+    Transform groundCheck;
+
+    public float jumpForce = 700;
     void Start()
     {
-        
+        ini = gameObject.GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("EnemyGroundCheck");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        noChao = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (plantaforma) {
+            if (!noChao) {
+                velocidade *= -1;
+            }
+        } else {
+            if (noChao) {
+                velocidade *= -1;
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        ini.velocity = new Vector2(velocidade, ini.velocity.y);
+        if(velocidade > 0 && facingRight) {
+            Flip();
+        }else if( velocidade < 0 && !facingRight) {
+            Flip();
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D obj) {
+        if (obj.gameObject.CompareTag("Player")) {
+            BoxCollider2D[] boxes = gameObject.GetComponents<BoxCollider2D>();
+            foreach(BoxCollider2D box in boxes) {
+                box.enabled = false;
+            }
+
+            obj.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            velocidade = 0;
+            transform.Rotate(new Vector3(0, 0, -180));
+            Destroy(gameObject, 2);
+        }
     }
 }
---
+++ Projeto/PI-Game/Assets/Scripts/inimigoNoChao.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigoNoChao: MonoBehaviour {
+    public float velocidade2;
+    Rigidbody2D iniNoChao;
+    bool facingRight2 = false;
+    bool noChao2 = false;
+    Transform groundCheck2;
+
+    public float jumpForce2 = 700;
+    void Start() {
+        iniNoChao = gameObject.GetComponent<Rigidbody2D>();
+        groundCheck2 = transform.Find("EnemyGroundCheck");
+    }
+
+    void Update() {
+        noChao2 = Physics2D.Linecast(transform.position, groundCheck2.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (noChao2) {
+            velocidade2 *= -1;
+        }
+    }
+
+    void FixedUpdate() {
+        iniNoChao.velocity = new Vector2(velocidade2, iniNoChao.velocity.y);
+        if (velocidade2 > 0 && facingRight2) {
+            flip();
+        } else if (velocidade2 < 0 && !facingRight2) {
+            flip();
+        }
+    }
+
+    void flip() {
+        facingRight2 = !facingRight2;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D obj) {
+        if (obj.gameObject.CompareTag("Player")) {
+            BoxCollider2D[] boxes = gameObject.GetComponents<BoxCollider2D>();
+            foreach (BoxCollider2D box in boxes) {
+                box.enabled = false;
+            }
+
+            obj.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce2));
+            velocidade2 = 0;
+            transform.Rotate(new Vector3(0, 0, -180));
+            Destroy(gameObject, 2);
+        }
+    }
+}
