5a5e71c698ff50c8b71440b275ed62ec2376da5e
J-Eugenio <eugeniopb4@gmail.com>
2019-04-09 16:47:16 -0300

    Ajustes

---
+++ Projeto/PI-Game/Assets/Scripts/GlossarioController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GlossarioController : MonoBehaviour
+{
+    public void Exit() {
+        Application.Quit();
+    }
+    public void JogarLv1() {
+        StartCoroutine(carregarScenaLv1());
+    }
+    public void Voltar() {
+        StartCoroutine(carregarVoltar());
+    }
+    IEnumerator carregarScenaLv1() {
+        yield return new WaitForSeconds(0);
+        SceneManager.LoadScene("Fase1");
+    }
+    IEnumerator carregarVoltar() {
+        yield return new WaitForSeconds(0);
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+}
