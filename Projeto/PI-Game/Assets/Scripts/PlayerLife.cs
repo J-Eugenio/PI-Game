2982d2878ece01e304002d95f2b90ce272da2201
DawnGrade <37818718+ThiagoAlencar12@users.noreply.github.com>
2019-04-09 16:24:00 -0300

    Alterações
    Save checkpoint

--- Projeto/PI-Game/Assets/Scripts/PlayerLife.cs
+++ Projeto/PI-Game/Assets/Scripts/PlayerLife.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
+    public GameObject lastCheckpoint;
     Animator anim;
     bool vivo = true;
     public Vector3 respawnPoint;
+
 
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         GameManager.gm.AtualizaHud();
+        
     }
 
     // Update is called once per frame
@@ -20,33 +23,46 @@ public class PlayerLife : MonoBehaviour
     {
         
     }
-    //setando se ele tocar em alguma tag com o nome ele aparece no reapwan
-    void OnTriggerEnter2D(Collider2D other)
+    // //setando se ele tocar em alguma tag com o nome ele aparece no reapwan
+    //void OnTriggerEnter2D(Collider2D other)
+    // {
+    //aqui é para quando ele morre, vir para o check point atual
+    //if (other.tag == "")
+    // {
+    // transform.position = respawnPoint;
+    // }
+    //aqui é marcando, se ele chegou no checkpoint, ele salva.
+    // if(other.tag == "Checkpoint")
+    // {
+    //  respawnPoint = other.transform.position;
+    //}
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        //aqui é para quando ele morre, vir para o check point atual
-        if (other.tag == "")
-        {
-            transform.position = respawnPoint;
-        }
-        //aqui é marcando, se ele chegou no checkpoint, ele salva.
-        if(other.tag == "Checkpoint")
-        {
-            respawnPoint = other.transform.position;
-        }
+        lastCheckpoint = collider2D.gameObject;
     }
-        public void PerdeVida() {
+
+
+    public void PerdeVida() {
         if (vivo) {
             vivo = false;
             anim.SetTrigger("Morreu");
             GameManager.gm.SetVidas(-1);
             gameObject.GetComponent<PlayerAttack>().enabled = false;
             gameObject.GetComponent<PlayerController>().enabled = false;
+
         }
+        
     }
 
     public void Reset() {
         if(GameManager.gm.GetVidas() >= 0) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            gameObject.GetComponent<PlayerAttack>().enabled = true;
+            gameObject.GetComponent<PlayerController>().enabled = true;
+            transform.position = lastCheckpoint.transform.position;
         }
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
     }
 }
