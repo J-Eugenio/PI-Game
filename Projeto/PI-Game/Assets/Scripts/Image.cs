179a64cc8ff1f077c3db3d1314e547b433a2a842
DawnGrade <37818718+ThiagoAlencar12@users.noreply.github.com>
2019-04-15 17:43:49 -0300

    Ataque e bordas ajustes
    Ultimos ajustes

---
+++ Projeto/PI-Game/Assets/Scripts/Image.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Image : MonoBehaviour
+{
+    [SerializeField] private Image imagem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            imagem.enabled = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            imagem.enabled = false;
+        }
+
+        
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
