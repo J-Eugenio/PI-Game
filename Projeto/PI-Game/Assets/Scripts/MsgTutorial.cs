4c31219ac6cdd6b041aa245d6bcf30122d89b4cf
J-Eugenio <eugeniopb4@gmail.com>
2019-04-27 12:46:03 -0300

    Msg tutorial Ajuste

---
+++ Projeto/PI-Game/Assets/Scripts/MsgTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MsgTutorial : MonoBehaviour
+{
+    public Renderer rend;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player"))
+            rend.enabled = true;
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player"))
+            rend.enabled = false;
+    }
+}
