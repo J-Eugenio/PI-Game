91973646a544c2a257ac70fabc1893aaccb703a9
J-Eugenio <eugeniopb4@gmail.com>
2019-05-04 20:36:14 -0300

    Começo do Puzzle(memoria), a terminar

---
+++ Projeto/PI-Game/Assets/Scripts/AddButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzlecampo;
+
+    [SerializeField]
+    private GameObject btn;
+
+    void Awake() {
+        for(int i = 0; i < 8; i++) {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzlecampo, false);
+        }
+    }
+}
---
+++ Projeto/PI-Game/Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite bgImage;
+    public List<Button> btns = new List<Button>();
+
+    void Start() {
+        GetButtons();
+    }
+    void GetButtons() {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+        for(int i = 0; i < objects.Length; i++) {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+}
