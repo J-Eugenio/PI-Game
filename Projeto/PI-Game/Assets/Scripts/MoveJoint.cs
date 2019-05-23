554f914721877c068c4cd1f1cb51c5bdd12e69ad
J-Eugenio <eugeniopb4@gmail.com>
2019-05-23 16:45:14 -0300

    Plantaforma Movel

---
+++ Projeto/PI-Game/Assets/Scripts/MoveJoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveJoint : MonoBehaviour
+{
+    [SerializeField]
+    private SliderJoint2D slider;
+    [SerializeField]
+    private JointMotor2D aux;
+
+    void Start()
+    {
+        aux = slider.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slider.limitState == JointLimitState2D.LowerLimit) {
+            aux.motorSpeed = 1;
+            slider.motor = aux;
+        }
+        if (slider.limitState == JointLimitState2D.UpperLimit) {
+            aux.motorSpeed = -1;
+            slider.motor = aux;
+        }
+    }
+}
