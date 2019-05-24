ffabc6802ceb84f5c18573ee84cba7c7f7031dea
DawnGrade <37818718+ThiagoAlencar12@users.noreply.github.com>
2019-05-24 01:34:22 -0300

    Quase fim, mais tade termino.
    Se tiver bug nos objetos revertam o comit, porque deu bug no meu editor, n sei pq, editor lixo eauhaeu mec

---
+++ Projeto/PI-Game/Assets/Scripts/MoverPlataforma.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPlataforma : MonoBehaviour
+{
+    public GameObject[] locais;
+    public int destinoInicial = 0;
+    public float velocidade = 10;
+    public bool comecarInvertido;
+    public bool reiniciarSequencia;
+
+    int localAtual = 0;
+    bool inverter = false;
+
+    void Start()
+    {
+        if (destinoInicial < locais.Length)
+        {
+            localAtual = destinoInicial;
+        }
+        else
+        {
+            localAtual = 0;
+        }
+        //
+        if (comecarInvertido == true)
+        {
+            inverter = true;
+        }
+    }
+
+    void Update()
+    {
+        if (inverter == false)
+        {
+            if (Vector3.Distance(transform.position, locais[localAtual].transform.position) < 0.1f)
+            {
+                if (localAtual < locais.Length - 1)
+                {
+                    localAtual++;
+                }
+                else
+                {
+                    if (reiniciarSequencia == true)
+                    {
+                        localAtual = 0;
+                    }
+                    else
+                    {
+                        inverter = true;
+                    }
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, locais[localAtual].transform.position, velocidade * Time.deltaTime);
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, locais[localAtual].transform.position) < 0.1f)
+            {
+                if (localAtual > 0)
+                {
+                    localAtual--;
+                }
+                else
+                {
+                    if (reiniciarSequencia == true)
+                    {
+                        localAtual = locais.Length - 1;
+                    }
+                    else
+                    {
+                        inverter = false;
+                    }
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, locais[localAtual].transform.position, velocidade * Time.deltaTime);
+        }
+    }
+}
